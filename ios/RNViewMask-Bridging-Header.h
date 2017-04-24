ebdec50a869d5bcd7a8ecbb4c9a51cc32fc9b071
Rikard Franksson <rikardfranksson@Cirala-Pro.local>
2017-04-24 11:22:50 +0200

    create folder

--- ios/RNViewMask-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-#ifndef RNViewMask_Bridging_Header_h
-#define RNViewMask_Bridging_Header_h
-
-#import "React/RCTBridgeModule.h"
-#import "React/RCTViewManager.h"
-
-#endif /* RNViewMask_Bridging_Header_h */
--- ios/RNViewMaskManager.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-@objc(RNViewMaskManager)
-class RNViewMaskManager: RCTViewManager {
-  
-  @objc override func view() -> UIView! {
-    return RNViewMask()
-  }
-}
