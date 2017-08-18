3bcd4e5ff6ee5c6a14a7d144d849046dc21f6d5f
Ben Lambert <ben@blam.sh>
2017-08-18 10:57:06 +0200

    Update RNViewMaskPackage.java

--- android/src/main/java/com/reactlibrary/RNViewMaskPackage.java
+++ android/src/main/java/com/reactlibrary/RNViewMaskPackage.java
@@ -20,11 +20,6 @@ public class RNViewMaskPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return Collections.emptyList();
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Arrays.<ViewManager>asList(
                 new RNViewMaskModule(reactContext)
